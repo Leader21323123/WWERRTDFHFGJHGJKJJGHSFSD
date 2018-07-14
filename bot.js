52153ec5743a3ca3d42299f953e7366549b1dd08
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-14 16:43:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,6 +53,29 @@ client.on('message', message => {
          "\n" + "**" + args + "**")
      }
      });
-
+client.on('ebnklb',function(ebnklb) {
+     
+     if(ebnklb.content.startsWith("<@453902836133265410>")) {
+         ebnklb.channel.send('Hey Im **L-BOT!**  A Nice Bot Developed By:`<@285805483510726657>`')
+         ebnklb.channel.send('My Prefix `L-`')
+ 
+     }
+ });
+ client.on('message', ra3d => {
+ var prefix = "L-";
+                         let args = ra3d.content.split(" ").slice(1).join(" ")
+ if(ra3d.content.startsWith(prefix + 'cc')) {
+     if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+              if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+               ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                   setInterval(function(){})
+                     let count = 0;
+                     let ecount = 0;
+           for(let x = 1; x < `${parseInt(args)+1}`; x++){
+             ra3d.guild.createRole({name:x,
+               color: 'RANDOM'})
+               }
+             }
+        });
 
 client.login(process.env.BOT_TOKEN);
