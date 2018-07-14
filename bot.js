c192e328e55c3134bf29e7365f2b651f58d8ea11
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-14 16:11:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = 'L-'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`L-bot `,"http://twitch.tv/S-F")
+client.user.setGame(`L-inv `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -27,7 +27,44 @@ client.user.setGame(`L-bot `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-
-
+client.on('message', message => {
+    if(!message.channel.guild) return;
+ if(message.content.startsWith(prefix + 'clear')) {
+ if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
+ if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Do not have permission** `ADMINISTRATOR`' );
+ let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+ let request = `Requested By ${message.author.username}`;
+ message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
+ msg.react('✅')
+ .then(() => msg.react('❌'))
+ .then(() =>msg.react('✅'))
+ 
+ let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+ let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+ 
+ let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+ let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+ reaction1.on("collect", r => {
+ message.channel.send(`Chat will delete`).then(m => m.delete(5000));
+ var msg;
+         msg = parseInt();
+ 
+       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+       message.channel.sendMessage("", {embed: {
+         title: "`` Chat Deleted ``",
+         color: 0x06DF00,
+         footer: {
+ 
+         }
+       }}).then(msg => {msg.delete(3000)});
+ 
+ })
+ reaction2.on("collect", r => {
+ message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
+ msg.delete();
+ })
+ })
+ }
+ });
 
 client.login(process.env.BOT_TOKEN);
