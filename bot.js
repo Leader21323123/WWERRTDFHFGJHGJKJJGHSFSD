5a6e17e572609f8af4edc828b3cfd3e6d66091b2
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-15 14:30:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,11 +28,11 @@ client.user.setGame(`L-inv | L-bc `,"http://twitch.tv/S-F")
   console.log('')
 });
 client.on('message', message => {
-            if(!message.channel.guild) return;
+    if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('L-users')){ //الامر
- if(!message.author.id == '285805483510726657') return;
-message.channel.sendMessage('جار ارسال الرسالة |✅')
+if (message.content.startsWith('-bc-users')){
+if(!message.author.id === '285805483510726657') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
 })
