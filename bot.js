ac7f1d55aa8074c75f0afb3239a1a8a499c0c996
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-19 16:40:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -255,39 +255,8 @@ var day = message.guild.createdAt.getDate()
 } 
 
 });
-client.on("message", (message) => {
-    if (message.content.startsWith('L-del')) {
-        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
 
-        let args = message.content.split(' ').slice(1);
-        let channel = message.client.channels.find('name', args.join(' '));
-        if (!channel) return message.reply('**There is no room like this name -_-**').catch(console.error);
-        channel.delete()
-    }
-});
 
-client.on('message', message => {
-    if (message.content.startsWith("f!stats")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .addField('Uptime', timeCon(process.uptime()), true)
-            .addField('RAM Usage', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
-            .addField('Guild Count', client.guilds.size, true)
-    })
-}
-});
-
-function timeCon(time) {
-    let days = Math.floor(time % 31536000 / 86400)
-    let hours = Math.floor(time % 31536000 % 86400 / 3600)
-    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
-    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
-    days = days > 9 ? days : '0' + days
-    hours = hours > 9 ? hours : '0' + hours
-    minutes = minutes > 9 ? minutes : '0' + minutes
-    seconds = seconds > 9 ? seconds : '0' + seconds
-    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
-}
           client.on("message", message => {
             var prefix = "L-"
             if (message.content === prefix + "help") {
