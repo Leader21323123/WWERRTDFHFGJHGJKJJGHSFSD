5d04f364c8212a91e6c0a29be2548f63b957bf8f
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-23 16:08:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -185,19 +185,7 @@ client.on('message', message => {
      }
    });
 
-client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "obc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
   client.on("message", message => {
     var prefix ='L-';
      if (message.content === prefix + "help") {
